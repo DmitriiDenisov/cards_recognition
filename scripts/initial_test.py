fe9926d437a365c78de4f09e1b8b237d5c79836e
alvlandr <alvlandr@gmail.com>
2018-08-01 14:31:49 +0300

    update project

--- scripts/initial_test.py
+++ scripts/initial_test.py
@@ -1,47 +1,16 @@
-from keras.applications import mobilenet
+import os
+
+from keras.applications import vgg16, inception_v3, resnet50, mobilenet
 from keras.preprocessing import image
 from keras.layers.core import Dropout, Activation, Reshape
 from keras.layers import Conv2D
 from keras.models import Model
-from keras import optimizers, callbacks
-import os
-import tensorflow as tf
+from keras import optimizers
+from keras.callbacks import ModelCheckpoint, TensorBoard
+
 from matplotlib import pyplot as plt
 
-# python -m tensorboard.main --logdir ./
-# cd ./Users/ddenisov/PycharmProjects/cardsmobile_recognition/logs/
-
-tf.summary.FileWriterCache.clear()
-
-PROJECT_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-temp_path_run = os.path.join(PROJECT_PATH, 'logs', 'run')
-temp_path = temp_path_run + '1'
-i = 2
-while os.path.exists(temp_path):
-    temp_path = temp_path_run + str(i)
-    i += 1
-log_run_dir = temp_path
-
-NUM_CLASSES = 3
-
-#sess = tf.Session()
-#print(os.path.join(PROJECT_PATH, 'logs'))
-#writer = tf.summary.FileWriter(os.path.join(PROJECT_PATH, 'logs'), sess.graph)
-#sess = tf.InteractiveSession()
-#writer = tf.summary.FileWriter(os.path.join(PROJECT_PATH, 'logs'))
-#writer.add_graph(sess.graph)
-
-
-print(log_run_dir)
-#tbCallBack = callbacks.TensorBoard(log_dir=os.path.join(PROJECT_PATH, 'logs'), histogram_freq=0,
-#                            write_graph=True, write_images=True)
-
-tbCallBack = callbacks.TensorBoard(log_dir=log_run_dir, histogram_freq=0, batch_size=32, write_graph=True,
-                            write_grads=False, write_images=False, embeddings_freq=0, embeddings_layer_names=None,
-                            embeddings_metadata=None, embeddings_data=None)
-
-sess = tf.InteractiveSession()
-tf.summary.FileWriter(log_run_dir, sess.graph)
+NUM_CLASSES = 2663
 
 # vgg_model = vgg16.VGG16(weights='imagenet')
 # inception_model = inception_v3.InceptionV3(weights='imagenet')
@@ -53,7 +22,7 @@ mobilenet_model.layers.pop()
 mobilenet_model.layers.pop()
 mobilenet_model.layers.pop()
 mobilenet_model.get_layer(name='reshape_1').name='reshape_0'
-o = Conv2D(filters=NUM_CLASSES, kernel_size=(1, 1))(mobilenet_model.layers[-1].output)
+o = Conv2D(filters=NUM_CLASSES, kernel_size=(1,1))(mobilenet_model.layers[-1].output)
 o = Activation('softmax', name='loss')(o)
 o = Reshape((NUM_CLASSES,))(o)
 # o.layers[-1].name = 'reshape_2'
@@ -73,43 +42,48 @@ valid_datagen= image.ImageDataGenerator(
         horizontal_flip=True)
 
 train_generator = train_datagen.flow_from_directory(
-    directory=r"../resource/samples/train",
+    directory=r"../resource/train",
     target_size=(224, 224),
     color_mode="rgb",
-    batch_size=32,
+    batch_size=64,
     class_mode="categorical",
     shuffle=True,
     seed=42
 )
 valid_generator = valid_datagen.flow_from_directory(
-    directory=r"../resource/samples/test",
+    directory=r"../resource/test",
     target_size=(224, 224),
     color_mode="rgb",
-    batch_size=32,
+    batch_size=64,
     class_mode="categorical",
     shuffle=True,
     seed=42
 )
 
 
-sgd = optimizers.SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
-mod_model.compile(optimizer=sgd,
+# sgd = optimizers.SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
+adam = optimizers.Adam(lr=0.1)# , decay=1e-6, momentum=0.9, nesterov=True)
+mod_model.compile(optimizer=adam,
               loss='categorical_crossentropy',
               metrics=['accuracy'])
 
 
-STEP_SIZE_TRAIN = 2  # train_generator.n//train_generator.batch_size
-STEP_SIZE_VALID = 2  # valid_generator.n//valid_generator.batch_size
+STEP_SIZE_TRAIN = train_generator.n//train_generator.batch_size
+STEP_SIZE_VALID = valid_generator.n//valid_generator.batch_size
+
+PROJECT_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+tbCallBack = TensorBoard(log_dir=os.path.join(PROJECT_PATH, 'logs'), histogram_freq=0, write_graph=True, write_images=True)
+checkpointer = ModelCheckpoint('my_model.h5', monitor='val_loss', verbose=0, save_best_only=False, save_weights_only=False, mode='auto', period=1)
+
+
 history = mod_model.fit_generator(generator=train_generator,
                     steps_per_epoch=STEP_SIZE_TRAIN,
                     validation_data=valid_generator,
                     validation_steps=STEP_SIZE_VALID,
                     epochs=10,
-                    use_multiprocessing=False, callbacks=[tbCallBack]
+                    callbacks=[checkpointer, tbCallBack]
 )
 
-saver = tf.train.Saver()
-saver.save(sess, log_run_dir)
 
 history.model.save('my_model.h5')
 
