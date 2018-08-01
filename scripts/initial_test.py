0cdf8729e9cb147967bf8f3dc9028b0c1b7e4338
alvlandr <alvlandr@gmail.com>
2018-08-01 14:24:41 +0300

    classifier script updated

--- scripts/initial_test.py
+++ scripts/initial_test.py
@@ -1,13 +1,16 @@
+import os
+
 from keras.applications import vgg16, inception_v3, resnet50, mobilenet
 from keras.preprocessing import image
 from keras.layers.core import Dropout, Activation, Reshape
 from keras.layers import Conv2D
 from keras.models import Model
 from keras import optimizers
+from keras.callbacks import ModelCheckpoint, TensorBoard
 
 from matplotlib import pyplot as plt
 
-NUM_CLASSES = 601
+NUM_CLASSES = 2663
 
 # vgg_model = vgg16.VGG16(weights='imagenet')
 # inception_model = inception_v3.InceptionV3(weights='imagenet')
@@ -42,7 +45,7 @@ train_generator = train_datagen.flow_from_directory(
     directory=r"../resource/train",
     target_size=(224, 224),
     color_mode="rgb",
-    batch_size=32,
+    batch_size=64,
     class_mode="categorical",
     shuffle=True,
     seed=42
@@ -51,27 +54,34 @@ valid_generator = valid_datagen.flow_from_directory(
     directory=r"../resource/test",
     target_size=(224, 224),
     color_mode="rgb",
-    batch_size=32,
+    batch_size=64,
     class_mode="categorical",
     shuffle=True,
     seed=42
 )
 
 
-sgd = optimizers.SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
-mod_model.compile(optimizer=sgd,
+# sgd = optimizers.SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
+adam = optimizers.Adam(lr=0.1)# , decay=1e-6, momentum=0.9, nesterov=True)
+mod_model.compile(optimizer=adam,
               loss='categorical_crossentropy',
               metrics=['accuracy'])
 
 
 STEP_SIZE_TRAIN = train_generator.n//train_generator.batch_size
 STEP_SIZE_VALID = valid_generator.n//valid_generator.batch_size
+
+PROJECT_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+tbCallBack = TensorBoard(log_dir=os.path.join(PROJECT_PATH, 'logs'), histogram_freq=0, write_graph=True, write_images=True)
+checkpointer = ModelCheckpoint('my_model.h5', monitor='val_loss', verbose=0, save_best_only=False, save_weights_only=False, mode='auto', period=1)
+
+
 history = mod_model.fit_generator(generator=train_generator,
                     steps_per_epoch=STEP_SIZE_TRAIN,
                     validation_data=valid_generator,
                     validation_steps=STEP_SIZE_VALID,
                     epochs=10,
-                    use_multiprocessing=True
+                    callbacks=[checkpointer, tbCallBack]
 )
 
 
